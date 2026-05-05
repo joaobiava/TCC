52d6fcde66576674079f54d63aea2bdb8a5c497b
jaba <joaobiava48@gmail.com>
2026-05-05 14:21:13 -0300

    aparentemente agr ta funfando, conferir pra ter ctz + adicionando parte de salvar o grafo

--- grafos.py
+++ grafos.py
@@ -1,32 +1,23 @@
 import networkx as nx
+from networkx import Graph
 import pandas as pd
 import matplotlib.pyplot as plt
-from networkx.algorithms import bipartite
 import glob
 import os
 import sys
 import time
-from memory_profiler import profile
+import pickle
 
-"""grafo bipartido (users + items) 
-faz de um jeito meio cagado, carregando tood o csv antes, 
-se o csv tivesse 10G ia cagar com tudo sem nem fazer nada do grafo em si"""
-def users_items(arquivos):
-    list_df = [pd.read_csv(arquivo) for arquivo in arquivos]
-    df = pd.concat(list_df , ignore_index=True)
-    users = df["user_id"]
-    items = df["click_article_id"]
+def salvar_grafo(G, caminho):
+    with open(caminho, 'wb') as f:
+        pickle.dump(G, f)
+        print(f"Grafo salvo em {caminho}")
 
-    G = nx.Graph()
-
-    G.add_nodes_from(users)
-    G.add_nodes_from(items)
-
-    G.add_edges_from(zip(users, items))
-
-    print(G)
-    del list_df
-    del df
+def carregar_grafo(caminho):
+    with open(caminho, 'rb') as f:
+        G = pickle.load(f)
+        print(f"Grafo carregado {G}")
+        return G
 
 """ 
 grafo bipartido (users + items)
@@ -38,41 +29,45 @@ def users_items_intertuples(arquivos):
 
     # usando dtype para diminuir o uso de memória, pq por padrão o pandas usa um valor maior
     for arquivo in arquivos:
-        df = pd.read_csv(arquivo, usecols=['user_id', 'click_article_id'], dtype={'user_id': 'int64', 'click_article_id': 'int64'})
+        # talvez mudar o tipo da variavel para ocupar menos espaco n seja interessante nesse metodo, pq eh apagado a cada evz
+        df = pd.read_csv(arquivo, usecols=['user_id', 'click_article_id'], dtype={'user_id': 'uint64', 'click_article_id': 'uint64'})
 
         #itertuples é mais rápido que interrows
         for row in df.itertuples():
-            u = row[1]
-            i = row[2]
-            G.add_node(u, subset=0)
-            G.add_node(i, subset=1)
-            G.add_edge(u, i)
+            # tem q fazer assim pra nao duplicar dados nessa bomba
+            user = f"u_{row[1]}"
+            item = f"i_{row[2]}"
+            G.add_node(user, subset=0, tipo="user")
+            G.add_node(item, subset=1, tipo="item")
+            G.add_edge(user, item)
 
         del df
-
     print(G)
-   # aparentemente deu certo
+    return G
+   # deu certo e com a quantidade certas de nos totais
 
-def users_items_sessions_localizations_devices_intertuples(arquivos):
+def users_items_sessions_devices_regions_intertuples(arquivos):
     G = nx.Graph()
 
     # usando dtype para diminuir o uso de memória, pq por padrão o pandas usa um valor maior
     for arquivo in arquivos:
-        df = pd.read_csv(arquivo, usecols=['user_id', 'click_article_id', 'session_id', 'click_deviceGroup', 'click_region'], 
+        df = pd.read_csv(arquivo, usecols=['user_id', 'session_id', 'click_article_id', 'click_deviceGroup', 'click_region'], 
                          dtype={'user_id': 'uint64', 'click_article_id': 'uint64', 'session_id': 'uint64', 'click_deviceGroup': 'uint8', 'click_region': 'uint8'})
 
         #itertuples é mais rápido que interrows
+        # essa bomba deu erro pq funciona de acordo com o csv, e nao a ordem escolhida ali em cima T-T
         for row in df.itertuples():
-            users = row[1]
-            items = row[2]
-            sessions = row[3]
-            devices = row[4]
-            regions = row[5]
-            G.add_node(users, subset=0)
-            G.add_node(sessions, subset=1)
-            G.add_node(items, subset=2)
-            G.add_node(devices, subset=3)
-            G.add_node(regions, subset=4)
+            users = f"u_{row[1]}"
+            sessions = f"s_{row[2]}"
+            items = f"i_{row[3]}"
+            devices = f"d_{row[4]}"
+            regions = f"r_{row[5]}"
+            
+            G.add_node(users, subset=0, tipo="user")
+            G.add_node(sessions, subset=1, tipo="session")
+            G.add_node(items, subset=2, tipo="item")
+            G.add_node(devices, subset=3, tipo="device")
+            G.add_node(regions, subset=4, tipo="region")
 
             G.add_edge(users, sessions)
             G.add_edge(sessions, items)
@@ -82,153 +77,96 @@ def users_items_sessions_localizations_devices_intertuples(arquivos):
         del df
 
     print(G)
+    return G
    # aparentemente deu certo
 
-# grafo multipartido (users + sessions + items)
-def users_sessions_items(df):
-    users = df["user_id"]
-    items = df["click_article_id"]
-    sessions = df["session_id"]
-    G = nx.Graph()
 
-    G.add_nodes_from(users, subset=0)
-    G.add_nodes_from(items, subset = 2)
-    G.add_nodes_from(sessions, subset = 1)
+def recomendar_com_rwr(G: Graph, user_id, top_k, nao_alpha=0.2):
+    if user_id not in G:
+        print(f"Usuário {user_id} não encontrado no grafo.")
+        return []
 
-    G.add_edges_from(zip(users, sessions))
-    G.add_edges_from(zip(sessions, items))
+    # so o user alvo recebe peso 1
+    personalization_target_user = {node: 0 for node in G.nodes()}
+    personalization_target_user[user_id] = 1
 
-    print(G)
+    # RWR
+    scores = nx.pagerank(G, alpha=1 - nao_alpha, personalization=personalization_target_user, max_iter=200)
 
-    degrees = dict(G.degree())
-    top_3_nodes = sorted(degrees.items(), key=lambda x: x[1], reverse=True)[:3]
-    print("top 3 nós")
-    for node, degree in top_3_nodes:
-        print(f"  {node}: {degree} conexões")
+    target_user_neighbors = list(G.neighbors(user_id))
+    is_session = any(G.nodes[v].get('tipo') == 'session' for v in target_user_neighbors)
 
-    sys.exit(0)
+    # itens que o user ja clicou
+    clicked = set()
+    if is_session:
+        sessions = [v for v in target_user_neighbors if G.nodes[v].get('tipo') == 'session']
+        for s in sessions:
+            for neighbors in G.neighbors(s):
+                if G.nodes[neighbors].get('tipo') == 'item':
+                    clicked.add(neighbors)
+    else:
+        for neighbors in G.neighbors(user_id):
+            if G.nodes[neighbors].get('tipo') == 'item':
+                clicked.add(neighbors)
 
-    plt.figure(figsize=(10, 10))
+    # Filtra itens q o usuario ainda n viu
+    recommendations = []
+    for node, score in scores.items():
+        if node not in clicked and G.nodes[node].get('tipo') == 'item':
+            recommendations.append((node, score))
 
-    pos = nx.multipartite_layout(G, subset_key="subset", scale=10)
-
-    nx.draw(G, pos, with_labels=True, node_size=50)
-
-    plt.title("Grafo user -> item")
-    plt.show()
-
-
-# grafo multipartido (devices + users)
-def devices_users(df):
-    # Adiciona prefixo para diferenciar os nós
-    users = ["user_" + str(u) for u in df["user_id"]]
-    os_list = ["os_" + str(o) for o in df["click_os"]]
-
-    G = nx.Graph()
-    G.add_nodes_from(users, bipartite=0)
-    G.add_nodes_from(os_list, bipartite=1)
-    G.add_edges_from(zip(users, os_list))
-
-    print(G)
-    plt.figure(figsize=(10, 10))
-
-    user_nodes = set(users)
-    pos = nx.bipartite_layout(G, user_nodes, scale=10)
-    nx.draw(G, pos, with_labels=True, node_size=50)
-    plt.title("Grafo users -> OS")
-    plt.show()
-    # predominantemente IOs, firefox e chromecast
-
-# grafo devices (users + os)
-def users_os(df):
-    users = ["user_" + str(u) for u in df["user_id"]]
-    os_list = ["os_" + str(o) for o in df["click_os"]]
-    G = nx.Graph()
-    G.add_nodes_from(users, bipartite=0)
-    G.add_nodes_from(os_list, bipartite=1)
-    G.add_edges_from(zip(users, os_list))
-
-    print(G)
-    plt.figure(figsize=(10, 10))
-
-    user_nodes = set(users)
-    pos = nx.bipartite_layout(G, user_nodes, scale=10)
-    nx.draw(G, pos, with_labels=True, node_size=50)
-    plt.title("Grafo devices -> OS")
-    plt.show()
-
-
-def users_regions(df):
-    users = ["user_" + str(u) for u in df["user_id"]]
-    region_list = ["region_" + str(o) for o in df["click_region"]]
-    G = nx.Graph()
-    G.add_nodes_from(users, bipartite=0)
-    G.add_nodes_from(region_list, bipartite=1)
-    G.add_edges_from(zip(users, region_list))
-
-    print(G)
-
-    degrees = dict(G.degree())
-    top_3_nodes = sorted(degrees.items(), key=lambda x: x[1], reverse=True)[:3]
-    print("top 3 nós")
-    for node, degree in top_3_nodes:
-        print(f"  {node}: {degree} conexões")
-
-    plt.figure(figsize=(10, 10))
-
-    user_nodes = set(users)
-    pos = nx.bipartite_layout(G, user_nodes, scale=10)
-    nx.draw(G, pos, with_labels=True, node_size=50)
-    plt.title("Grafo users -> regions")
-    plt.show()
-
-
-def session_interaction(df):
-    sessions = ["session_" + str(u) for u in df["session_id"]]
-    interactions = ["interaction_" + str(o) for o in df["session_size"]]
-    G = nx.Graph()
-    G.add_nodes_from(sessions, bipartite=0)
-    G.add_nodes_from(interactions, bipartite=1)
-    G.add_edges_from(zip(sessions, interactions))
-
-    print(G)
-
-    plt.figure(figsize=(10, 10))
-
-    session_nodes = set(sessions)
-    pos = nx.bipartite_layout(G, session_nodes, scale=10)
-    nx.draw(G, pos, with_labels=True, node_size=50)
-    plt.title("Grafo users -> regions")
-    plt.show()
-
+    # ordena itens e retorna os top-k
+    recommendations.sort(key=lambda x: x[1], reverse=True)
+    return recommendations[:top_k]
 
 
 if __name__ == "__main__":
+    caminho_grafo_simples = '/home/jaba/Documentos/TCC/grafo_simples'
+    caminho_grafo_completo = '/home/jaba/Documentos/TCC/grafo_completo'
     pasta = '/home/jaba/Documentos/TCC/clicks'
     arquivos = glob.glob(os.path.join(pasta, "*.csv"))
 
-    #execucao com o csv inteiro
-    inicio = time.perf_counter()
-    users_items(arquivos)
-    fim = time.perf_counter()
-    print(f"funcao users_items demorou {fim - inicio} segundos")
+    # funfou essa parte de carregar o grafo
+    # if os.path.exists(caminho_grafo_simples):
+    #     G = carregar_grafo(caminho_grafo_simples)
+    # else:
+    #     #execucao com liberacao de memoria
+    #     inicio = time.perf_counter()
+    #     G = users_items_intertuples(arquivos)
+    #     fim = time.perf_counter()
+    #     print(f"funcao users_items_intertuples demorou {fim - inicio} segundos")
+    #     salvar_grafo(G, caminho_grafo_simples)
 
-    #execucao com liberacao de memoria
-    inicio = time.perf_counter()
-    users_items_intertuples(arquivos)
-    fim = time.perf_counter()
-    print(f"funcao users_items_intertuples demorou {fim - inicio} segundos")
+    # #executa rwr no grafo mais simples (users + items)
+    # inicio = time.perf_counter()
+    # recomendacoes = recomendar_com_rwr(G, user_id="u_0", top_k=10)
+    # print(f"Recomendações para usuário 0:")
+    # for artigo, score in recomendacoes:
+    #     print(f"\tArtigo {artigo} — score: {score}")
+    # fim = time.perf_counter()
+    # print(f"funcao rwr demorou {fim - inicio} segundos")
 
+    # sys.exit(0)
+
+    #execucao liberando memoria (campos escolhidos pro grafo final)
+    if os.path.exists(caminho_grafo_completo):
+        G = carregar_grafo(caminho_grafo_completo)
+    else:
+        #execucao com liberacao de memoria
+        inicio = time.perf_counter()
+        G = users_items_sessions_devices_regions_intertuples(arquivos)
+        fim = time.perf_counter()
+        print(f"funcao users_items_sessions_localizations_devices_intertuples demorou {fim - inicio} segundos")
+        salvar_grafo(G, caminho_grafo_completo)
+        
+    # executa rwr no grafo mais complexo
     inicio = time.perf_counter()
-    users_items_sessions_localizations_devices_intertuples(arquivos)
+    recommendations = recomendar_com_rwr(G, user_id="u_0", top_k=20)
+    print(f"Recomendações para usuário u_0:")
+    for artigo, score in recommendations:
+        print(f"\tArtigo {artigo} — score: {score}")
     fim = time.perf_counter()
-    print(f"funcao users_items_sessions_localizations_devices_intertuples demorou {fim - inicio} segundos")
+    print(f"funcao rwr no grafo grande demorou {fim - inicio} segundos")
 
     #tempo entre as duas funccoes quase igual, porem da segunda tem liberacao de memoria do df
-    # importante lembrar que nao esta sendo usada uma biblioteca muito especifica para benchmark
-
-    #users_sessions_items(df)
-    #devices_users(df)
-    #users_os(df)
-    #users_regions(df)
-    #session_interaction(df)
+    # importante lembrar que nao esta sendo usada uma biblioteca muito especifica de tempo para o benchmark
