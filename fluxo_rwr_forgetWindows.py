eb8e3ab4625a2f235db655bd7898669e486bb90b
jaba <joaobiava48@gmail.com>
2026-05-17 17:59:12 -0300

    adicionei precision e ndcg + separei um pouco mais o codigo + otimizacao do build_subGraph + tipo nao sao mais strings (so acho)

--- fluxo_rwr_forgetWindows.py
+++ fluxo_rwr_forgetWindows.py
@@ -6,15 +6,29 @@ import os
 import sys
 import time
 import logging
-from models_and_save_graphs import existing_file, users_items, users_sessions_items, devices_users_sessions_items, devices_users_sessions_items_regions
+from models_and_save_graphs import (existing_file, users_items, users_sessions_items, 
+                                    devices_users_sessions_items, devices_users_sessions_items_regions)
+import math
+import bisect
+
+# variaveis 'tipo' montadas no grafo
+USER = 0
+ITEM = 1
+SESSION = 2
+REGION = 3
+DEVICE = 4
+USER_OFFSET = 1_000_000_000
+ITEM_OFFSET = 2_000_000_000
+SESSION_OFFSET = 3_000_000_000
+REGION_OFFSET = 4_000_000_000
+DEVICE_OFFSET = 5_000_000_000
 
 #deixei umas config global aq, mais facil
-USER_ID = "u_1709"
+USER_ID = USER_OFFSET + 1709
 TOP_KS = [5, 10, 15, 20]
 MS_DAY = 1000 * 60 * 60 * 24  # 1 dia em timestamp
 TIME_WINDOW = MS_DAY * 2 # 2 dias para fazer atualizacoes
 BETAS = [0.1, 0.15, 0.2, 0.25, 0.3]
-MIN_CLIQUES = 1000
 TS_CUTOFF = int(pd.Timestamp('2017-10-18').timestamp() * 1000)
 
 """
@@ -22,23 +36,23 @@ TS_CUTOFF = int(pd.Timestamp('2017-10-18').timestamp() * 1000)
 FUNCAO DE CRIAR SUBGRAFO PELO TIMESTAMP
 =============================================================================================
 """
-def build_subGraph(G: Graph, current_time, time_window):
-    lower_limit = current_time - time_window
+def build_subGraph(G, edges_by_time, timestamps_only, current_time):
+    lower_limit = current_time - TIME_WINDOW
 
+    left = bisect.bisect_left(timestamps_only, lower_limit)
 
-    selectedEdges = [(u, v) for u, v, data in G.edges(data=True)
-                     if lower_limit <= data.get('timestamp', 0) <= current_time]
-    
-    sub_graph = G.edge_subgraph(selectedEdges).copy()
+    right = bisect.bisect_right(timestamps_only, current_time)
 
-    return sub_graph
+    selectedEdges = [(u, v) for _, u, v in edges_by_time[left:right]]
+
+    return G.edge_subgraph(selectedEdges)
 
 """
 =============================================================================================
 FUNCAO DAR ATUALIZAR ITENS CLICADOS
 =============================================================================================
 """
-def update_clicked(G: Graph, clicked, user_id):
+def update_clicked(G: Graph, clicked):
     # target_user_neighbors = list(G.neighbors(user_id))
     # is_session = any(G.nodes[v].get('tipo') == 'session' for v in target_user_neighbors)
 
@@ -54,14 +68,12 @@ def update_clicked(G: Graph, clicked, user_id):
     #             clicked.add(neighbors)
 
 
-    for neighbor in G.neighbors(user_id):
-        tipo = G.nodes[neighbor].get('tipo')
-
-        if tipo == 'item':
+    for neighbor in G.neighbors(USER_ID):
+        if ITEM_OFFSET <= neighbor < SESSION_OFFSET:
             clicked.add(neighbor)
-        elif tipo =='session':
+        elif SESSION_OFFSET <= neighbor < REGION_OFFSET:
             for candidate in G.neighbors(neighbor):
-                if G.nodes[candidate].get('tipo') == 'item':
+                if ITEM_OFFSET <= candidate < SESSION_OFFSET:
                     clicked.add(candidate)
 
 """
@@ -69,12 +81,12 @@ def update_clicked(G: Graph, clicked, user_id):
 FUNCAO RwR
 =============================================================================================
 """
-def recommend_with_rwr_timeWindow(G: Graph, clicked, user_id, top_k, beta):
-    if(user_id not in G):
+def recommend_with_rwr_timeWindow(G: Graph, clicked, top_k, beta):
+    if(USER_ID not in G):
         print("user not found")
         return []
 
-    personalization_target_user = {user_id: 1}
+    personalization_target_user = {USER_ID: 1}
 
     # RWR
     scores = nx.pagerank(G, alpha=1 - beta, personalization=personalization_target_user)
@@ -82,7 +94,7 @@ def recommend_with_rwr_timeWindow(G: Graph, clicked, user_id, top_k, beta):
     # Filtra itens q o usuario ainda n viu
     recommendations = []
     for node, score in scores.items():
-        if node not in clicked and G.nodes[node].get('tipo') == 'item':
+        if node not in clicked and G.nodes[node].get('tipo') == ITEM:
             recommendations.append((node, score))
 
     # ordena itens e retorna os top-k
@@ -108,7 +120,7 @@ def ndcg_topK(recommended, relevants):
     dcg = 0.0
     for i, (item, _) in enumerate(recommended):
         if item in relevants:
-            dcg += 1 / (i + 2)  # log2(posição + 1), posição começa em 1
+            dcg += 1 / math.log2(i + 2)  # log2(posição + 1), posição começa em 1
 
     # ideal DCG — todos os acertos no topo da lista
     ideal_hits = min(len(relevants), len(recommended))
@@ -121,68 +133,98 @@ def ndcg_topK(recommended, relevants):
 EXECUTA TUDO AS FIRULA NA ORDEM CORRETA
 =============================================================================================
 """
-def execute_hiper(G, USER_ID, BETAS, TOP_KS, ts_begin, ts_end, time_window):
+def create_time_index(G):
+    edges_by_time = sorted(
+        [
+            (data["timestamp"], u, v)
+            for u, v, data in G.edges(data=True)
+            if "timestamp" in data
+        ]
+    )
+    # criado para fazer uma busca posteriormente onde fica mais rapido
+    timestamps_only = [
+        ts for ts, _, _ in edges_by_time
+    ]
+
+    return edges_by_time, timestamps_only
+
+def execute_time_window(G, ts_begin, ts_end, edges_by_time, timestamps_only, topK, beta):
+    clicked = set()
+    current_time = ts_begin + TIME_WINDOW
+
+    precisions = []
+    ndcgs = []
+    
+    # subgrafo criado aqui, pois entao posso igualar o future_subgraph, nao precisando calcular ele 2 vezes
+    inicio = time.perf_counter()
+    current_sub = build_subGraph(G, edges_by_time, timestamps_only, current_time)
+    fim = time.perf_counter()
+    print(f"demorou {fim - inicio} segundos para montar o subgrafo")
+
+    iteracao = 0
+    # se o tempo for maior que o do ultimo click do dataset ele para
+    while current_time <= ts_end:
+        print(f"\nITERACAO: {iteracao}")
+        iteracao+=1
+
+        timestamp_readable = pd.to_datetime(current_time, unit='ms')
+        print(f"tempo atual: {timestamp_readable}")
+
+        print(current_sub)
+        if USER_ID not in current_sub:
+            current_time += TIME_WINDOW
+            current_sub = build_subGraph(G, edges_by_time, timestamps_only, current_time)
+            continue
+
+        inicio = time.perf_counter()
+        future_sub = build_subGraph(G, edges_by_time, timestamps_only, current_time + TIME_WINDOW)
+        fim = time.perf_counter()
+        print(f"Demorou {fim - inicio} segundos para montar future_subgrafo")
+        print(future_sub)
+
+        future_clicked = set()
+        if USER_ID in future_sub:
+            update_clicked(future_sub, future_clicked)
+        
+        update_clicked(current_sub, clicked)
+        print(clicked)
+
+        relevants = future_clicked - clicked
+
+        #executa rwr
+        inicio = time.perf_counter()
+        recommended = recommend_with_rwr_timeWindow(current_sub, clicked, topK, beta)
+        fim = time.perf_counter()
+
+        print(f"[{timestamp_readable}] Recomendações para {USER_ID}")
+        for artigo, score in recommended:
+            print(f"\tArtigo {artigo} --- score: {score}")
+        print(f"funcao rwr demorou {fim - inicio} segundos")
+
+        if relevants:
+            precisions.append(precision_topK(recommended, relevants))
+            ndcgs.append(ndcg_topK(recommended, relevants))
+
+        # leve otmizacao pra nao precisar ficar fazendo o future sub muitas vezes
+        current_sub = future_sub
+        #pula pra proxima janela de tempo
+        current_time += TIME_WINDOW  #de acordo com a janela de tempo
+
+    return precisions, ndcgs
+
+def execute_hiperparams(G, ts_begin, ts_end):
     results = []
     total_combinations = len(BETAS) * len(TOP_KS)
     current_combination = 0
+    
+    edges_by_time, timestamps_only = create_time_index(G)
+    
     for beta in BETAS:
         for topK in TOP_KS:
-            clicked = set()
             current_combination += 1
-            current_time = ts_begin + time_window
-
             log.info(f"[{current_combination}/{total_combinations}] beta={beta} | top_k={topK}")
-
-            precisions = []
-            ndcgs = []
-
-            iteracao = 0
-            # se o tempo for maior que o do ultimo click do dataset ele para
-            while current_time <= ts_end:
-                print(f"\nITERACAO: {iteracao}")
-                iteracao+=1
-                timestamp_readable = pd.to_datetime(current_time, unit='ms')
-                print(f"tempo atual: {timestamp_readable}")
-
-                # constroi o subgrafo mesmo se o user-alvo nao estiver nele
-                inicio = time.perf_counter()
-                current_sub = build_subGraph(G, current_time, time_window)
-                future_sub = build_subGraph(G, current_time + time_window, time_window)
-                fim = time.perf_counter()
-                print(current_sub)
-                print(future_sub)
-                print(f"Demorou {fim - inicio} segundos para montar os subgrafos")
-
-                # se o user n tiver no subgrafo, da erro, caso queira alocar recomendacoes pra ele mesmo nao estando em sessao, obrigar a colcaor user no subgrafo
-                if(USER_ID not in current_sub):
-                    current_time += time_window
-                    continue
-
-                future_clicked = set()
-                if USER_ID in future_sub:
-                    update_clicked(future_sub, future_clicked, USER_ID)
-                
-                update_clicked(current_sub, clicked, USER_ID)
-                print(clicked)
-
-                relevants = future_clicked - clicked
-
-                #executa rwr
-                inicio = time.perf_counter()
-                recommended = recommend_with_rwr_timeWindow(current_sub, clicked, USER_ID, topK, beta)
-                fim = time.perf_counter()
-
-                print(f"[{timestamp_readable}] Recomendações para {USER_ID}")
-                for artigo, score in recommended:
-                    print(f"\tArtigo {artigo} --- score: {score}")
-                print(f"funcao rwr demorou {fim - inicio} segundos")
-
-                if relevants:
-                    precisions.append(precision_topK(recommended, relevants))
-                    ndcgs.append(ndcg_topK(recommended, relevants))
-
-                #pula pra proxima janela de tempo
-                current_time += time_window  #de acordo com a janela de tempo
+            
+            precisions, ndcgs = execute_time_window(G, ts_begin, ts_end, edges_by_time, timestamps_only, topK, beta)
 
             mean_precision = sum(precisions) / len(precisions) if precisions else 0
             mean_ndcg = sum(ndcgs) / len(ndcgs) if ndcgs else 0
@@ -222,7 +264,7 @@ if __name__ == "__main__":
     print("4 - grafo regions + users + sessions + devices + items")
     choice = int(input("escolha qual grafo deseja funfar\n"))
 
-    graph_name = {1: 'users_items', 2: 'users_sessions_items', 3: 'devices_users_sessions_items', 4: 'devices_users_sessions_regions_items'}[choice]
+    graph_name = {1: 'foget_users_items', 2: 'foget_users_sessions_items', 3: 'foget_devices_users_sessions_items', 4: 'foget_devices_users_sessions_regions_items'}[choice]
 
     logging.basicConfig(
         level=logging.INFO,
@@ -252,4 +294,4 @@ if __name__ == "__main__":
     ts_end = max(timestamps)
     current_time = ts_begin + TIME_WINDOW # já comeca com uma janela de dados
     
-    execute_hiper(G, USER_ID, BETAS, TOP_KS, ts_begin, ts_end, TIME_WINDOW)
+    execute_hiperparams(G, ts_begin, ts_end)
